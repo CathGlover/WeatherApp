2a25394ee35c2781f8832a4db1f24bb261cec0b2
CathGlover <117387100+CathGlover@users.noreply.github.com>
2023-01-11 07:05:39 +0000

    Added four day forecasts from SheCodes Api

--- codes/index.js
+++ codes/index.js
@@ -24,11 +24,9 @@ function currentCityTemperature(position) {
   let apiKey = "9d18ac1c378d20dc84d1fe2241698d6f";
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityLive}&units=metric&appid=${apiKey}`;
   axios.get(apiURL).then(showTemperature);
-
   let apiKeyTwo = "edf069311acf2bebo10f4bbbc53249t3";
   let apiURLTwo = `https://api.shecodes.io/weather/v1/forecast?query=${cityLive}&key=${apiKeyTwo}&units=metric`;
   axios.get(apiURLTwo).then(displayForecast);
-  console.log(apiURLTwo);
 }
 
 function showTemperature(event) {
@@ -57,27 +55,50 @@ function showTemperature(event) {
   celsiusTemperature = event.data.main.temp;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return day;
+}
+
 function displayForecast(event) {
   console.log(event);
   let forecastElementOne = document.querySelector("#day-1");
   forecastElementOne.innerHTML = "Forecast 1";
   let dateOne = document.querySelector("#temp-1");
-  dateOne.innerHTML = `${Math.round(event.data.daily[0].temperature.day)}°C`;
+  dateOne.innerHTML = `${Math.round(event.data.daily[1].temperature.day)}°C`;
+  let tempOne = document.querySelector("#day-1");
+  tempOne.innerHTML = formatDay(event.data.daily[1].time);
+  let iconOne = document.querySelector("#icon-1");
+  iconOne.setAttribute("src", `${event.data.daily[1].condition.icon_url}`);
 
   let forecastElementTwo = document.querySelector("#day-2");
   forecastElementTwo.innerHTML = "Forecast 2";
   let dateTwo = document.querySelector("#temp-2");
-  dateTwo.innerHTML = `${Math.round(event.data.daily[1].temperature.day)}°C`;
+  dateTwo.innerHTML = `${Math.round(event.data.daily[2].temperature.day)}°C`;
+  let tempTwo = document.querySelector("#day-2");
+  tempTwo.innerHTML = formatDay(event.data.daily[2].time);
+  let iconTwo = document.querySelector("#icon-2");
+  iconTwo.setAttribute("src", `${event.data.daily[2].condition.icon_url}`);
 
   let forecastElementThree = document.querySelector("#day-3");
   forecastElementThree.innerHTML = "Forecast 3";
   let dateThree = document.querySelector("#temp-3");
-  dateThree.innerHTML = `${Math.round(event.data.daily[2].temperature.day)}°C`;
+  dateThree.innerHTML = `${Math.round(event.data.daily[3].temperature.day)}°C`;
+  let tempThree = document.querySelector("#day-3");
+  tempThree.innerHTML = formatDay(event.data.daily[3].time);
+  let iconThree = document.querySelector("#icon-3");
+  iconThree.setAttribute("src", `${event.data.daily[3].condition.icon_url}`);
 
   let forecastElementFour = document.querySelector("#day-4");
   forecastElementFour.innerHTML = "Forecast 4";
   let dateFour = document.querySelector("#temp-4");
-  dateFour.innerHTML = `${Math.round(event.data.daily[3].temperature.day)}°C`;
+  dateFour.innerHTML = `${Math.round(event.data.daily[4].temperature.day)}°C`;
+  let tempFour = document.querySelector("#day-4");
+  tempFour.innerHTML = formatDay(event.data.daily[4].time);
+  let iconFour = document.querySelector("#icon-4");
+  iconFour.setAttribute("src", `${event.data.daily[4].condition.icon_url}`);
 }
 
 function getFarenheit(position) {
